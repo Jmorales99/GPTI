126f3cc607ebbf8b0fe7b4c968e2dc73838e33bf
FabsHazard <fcriveros@uc.cl>
2024-12-08 18:25:25 -0300

    image location cloudinary

--- src/adsData.js
+++ src/adsData.js
@@ -1,61 +1,61 @@
 const adsData = [
   {
     id: 0,
-    imageUrl: '/public/images/Casa.jpeg', // Updated to use an absolute path
+    imageUrl: 'https://res.cloudinary.com/fabshazard/image/upload/v1733692838/Casa_wmfvmc.jpg', // Updated to use an absolute path
     link: 'https://propiedades.elmercurio.com/propiedades/casa-en-venta-en-las-condes-cod47442179.html',
     title: 'Casa en Venta en Las Condes',
   },
   {
     id: 1,
-    imageUrl: '/public/images/Falabella.jpg',
+    imageUrl: 'https://res.cloudinary.com/fabshazard/image/upload/v1733692838/Falabella_exmnrm.jpg',
     link: 'https://www.falabella.com/falabella-cl',
     title: 'Ofertas Navideñas en Falabella',
   },
   {
     id: 2,
-    imageUrl: '/public/images/BurgerKing.webp',
+    imageUrl: 'https://res.cloudinary.com/fabshazard/image/upload/v1733692838/BurgerKing_xfmcic.webp',
     link: 'https://www.burgerking.cl/',
     title: 'Promoción Burger King',
   },
   {
     id: 3,
-    imageUrl: '/public/images/BurgerKing1.jpeg',
+    imageUrl: 'https://res.cloudinary.com/fabshazard/image/upload/v1733692839/BurgerKing1_hizbzi.jpg',
     link: 'https://www.burgerking.cl/cupones/?coupon=30-off-combo-bacon-king-doble&id=2372',
     title: '30% OFF Combo Bacon King Doble',
   },
   {
     id: 4,
-    imageUrl: '/public/images/BurgerKing2.jpeg',
+    imageUrl: 'https://res.cloudinary.com/fabshazard/image/upload/v1733692838/BurgerKing2_gyfsza.jpg',
     link: 'https://www.burgerking.cl/cupones/?coupon=30-off-combo-bacon-king-triple&id=3436/',
     title: '30% OFF Combo Bacon King Triple',
   },
   {
     id: 5,
-    imageUrl: '/public/images/Lenovo.jpeg',
+    imageUrl: 'https://res.cloudinary.com/fabshazard/image/upload/v1733692838/Lenovo_maezhy.jpg',
     link: 'https://www.lenovo.com/cl/es/d/ofertas/?orgRef=https%253A%252F%252Fcupon.cl%252F&cid=cl%3Aaffiliate%3A0qc2c4-tuscupones.com.mx&clickid=XGIUEfWHdxyKUB5VPCTaeUBZUkCQmZziRTdpSE0&irgwc=1',
     title: 'Descuento en Lenovo',
   },
   {
     id: 6,
-    imageUrl: '/public/images/Emma.jpeg',
+    imageUrl: 'https://res.cloudinary.com/fabshazard/image/upload/v1733692838/Emma_vrrbke.jpg',
     link: 'https://www.emma-colchon.cl/combos/?awc=25386_1732158709_0d787cb4dcd0ef739db2644979019932&utm_source=awin-wwwsavingsunitedcom&utm_medium=affiliate&utm_campaign=413377&utm_content=LTKxzilCqz3WXK6NEdFdQHrCInu8UJA',
     title: '70% OFF en Emma Conchones Chile',
   },
   {
     id: 7,
-    imageUrl: '/public/images/LG.jpeg',
+    imageUrl: 'https://res.cloudinary.com/fabshazard/image/upload/v1733692839/LG_lziqrc.jpg', // Updated to use Cloudinary URL
     link: 'https://www.lg.com/cl/ofertas-smart-tv/?cjdata=MXxOfDB8WXww&cjevent=6cfc1cb1a7b611ef8274a6c20a82b82d&UTM_Source=CJ&UTM_id=5024114&UTM_campaigns=Savings+United+LATAM&UTM_Medium=affiliate',
     title: '46% OFF LG',
   },
   {
     id: 8,
-    imageUrl: '/public/images/BarceloHotel.jpeg',
+    imageUrl: 'https://res.cloudinary.com/fabshazard/image/upload/v1733692838/BarceloHotel_bx1eyq.jpg',
     link: 'https://www.barcelo.com/es-cl/ofertas/?utm_id=275885868_128489544&utm_campaign=latam_es_afl_bc_x_alwayson-x-x-x-x-x-x_co&c_content=textlink&c_campaign=alwayson&c_term=2988890_&utm_medium=afi&utm_source=barcelo_affiliates&dclid=CJDZ2ZW67IkDFSFNuAQdelkE3Q',
     title: 'Hasta un 40% OFF en Barceló Hotel',
   },
   {
     id: 9,
-    imageUrl: '/public/images/Aliexpress.jpeg',
+    imageUrl: 'https://res.cloudinary.com/fabshazard/image/upload/v1733692839/Aliexpress_a1cwdr.jpg',
     link: 'https://best.aliexpress.com/?wh_offline=true&gatewayAdapt=glo2esp&browser_redirect=true',
     title: '$4.800 de descuento en Aliexpress',
   },
