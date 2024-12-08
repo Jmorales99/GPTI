e3c9ee4a58c13a1de19f3c09b72d1b6ac0ee51b3
FabsHazard <fcriveros@uc.cl>
2024-12-08 18:06:56 -0300

    ads data

--- src/adsData.js
+++ src/adsData.js
@@ -1,68 +1,64 @@
-// adsData.js
 const adsData = [
-    {
-      id: 0,
-      imageUrl: require('../public/images/Casa.jpeg'),
-      link: 'https://propiedades.elmercurio.com/propiedades/casa-en-venta-en-las-condes-cod47442179.html',
-      title: 'Casa en Venta en Las Condes',
-    },
+  {
+    id: 0,
+    imageUrl: '/images/Casa.jpeg', // Updated to use an absolute path
+    link: 'https://propiedades.elmercurio.com/propiedades/casa-en-venta-en-las-condes-cod47442179.html',
+    title: 'Casa en Venta en Las Condes',
+  },
+  {
+    id: 1,
+    imageUrl: '/images/Falabella.jpg',
+    link: 'https://www.falabella.com/falabella-cl',
+    title: 'Ofertas Navideñas en Falabella',
+  },
+  {
+    id: 2,
+    imageUrl: '/images/BurgerKing.webp',
+    link: 'https://www.burgerking.cl/',
+    title: 'Promoción Burger King',
+  },
+  {
+    id: 3,
+    imageUrl: '/images/BurgerKing1.jpeg',
+    link: 'https://www.burgerking.cl/cupones/?coupon=30-off-combo-bacon-king-doble&id=2372',
+    title: '30% OFF Combo Bacon King Doble',
+  },
+  {
+    id: 4,
+    imageUrl: '/images/BurgerKing2.jpeg',
+    link: 'https://www.burgerking.cl/cupones/?coupon=30-off-combo-bacon-king-triple&id=3436/',
+    title: '30% OFF Combo Bacon King Triple',
+  },
+  {
+    id: 5,
+    imageUrl: '/images/Lenovo.jpeg',
+    link: 'https://www.lenovo.com/cl/es/d/ofertas/?orgRef=https%253A%252F%252Fcupon.cl%252F&cid=cl%3Aaffiliate%3A0qc2c4-tuscupones.com.mx&clickid=XGIUEfWHdxyKUB5VPCTaeUBZUkCQmZziRTdpSE0&irgwc=1',
+    title: 'Descuento en Lenovo',
+  },
+  {
+    id: 6,
+    imageUrl: '/images/Emma.jpeg',
+    link: 'https://www.emma-colchon.cl/combos/?awc=25386_1732158709_0d787cb4dcd0ef739db2644979019932&utm_source=awin-wwwsavingsunitedcom&utm_medium=affiliate&utm_campaign=413377&utm_content=LTKxzilCqz3WXK6NEdFdQHrCInu8UJA',
+    title: '70% OFF en Emma Conchones Chile',
+  },
+  {
+    id: 7,
+    imageUrl: '/images/LG.jpeg',
+    link: 'https://www.lg.com/cl/ofertas-smart-tv/?cjdata=MXxOfDB8WXww&cjevent=6cfc1cb1a7b611ef8274a6c20a82b82d&UTM_Source=CJ&UTM_id=5024114&UTM_campaigns=Savings+United+LATAM&UTM_Medium=affiliate',
+    title: '46% OFF LG',
+  },
+  {
+    id: 8,
+    imageUrl: '/images/BarceloHotel.jpeg',
+    link: 'https://www.barcelo.com/es-cl/ofertas/?utm_id=275885868_128489544&utm_campaign=latam_es_afl_bc_x_alwayson-x-x-x-x-x-x_co&c_content=textlink&c_campaign=alwayson&c_term=2988890_&utm_medium=afi&utm_source=barcelo_affiliates&dclid=CJDZ2ZW67IkDFSFNuAQdelkE3Q',
+    title: 'Hasta un 40% OFF en Barceló Hotel',
+  },
+  {
+    id: 9,
+    imageUrl: '/images/Aliexpress.jpeg',
+    link: 'https://best.aliexpress.com/?wh_offline=true&gatewayAdapt=glo2esp&browser_redirect=true',
+    title: '$4.800 de descuento en Aliexpress',
+  },
+];
 
-    {
-      id: 1,
-      imageUrl: require('../public/images/Falabella.jpg'),
-      link: 'https://www.falabella.com/falabella-cl',
-      title: 'Ofertas Navideñas en Falabella',
-    },
-    
-    {
-      id: 2,
-      imageUrl: require('../public/images/BurgerKing.webp'),
-      link: 'https://www.burgerking.cl/',
-      title: 'Promoción Burger King',
-    },
-    {
-      id: 3,
-      imageUrl: require('../public/images/BurgerKing1.jpeg'),
-      link: 'https://www.burgerking.cl/cupones/?coupon=30-off-combo-bacon-king-doble&id=2372',
-      title: '30% OFF Combo Bacon King Doble',
-    },
-    {
-      id: 4,
-      imageUrl: require('../public/images/BurgerKing2.jpeg'),
-      link: 'https://www.burgerking.cl/cupones/?coupon=30-off-combo-bacon-king-triple&id=3436/',
-      title: '30% OFF Combo Bacon King Triple',
-    },
-    {
-      id: 5,
-      imageUrl: require('../public/images/Lenovo.jpeg'),
-      link: 'https://www.lenovo.com/cl/es/d/ofertas/?orgRef=https%253A%252F%252Fcupon.cl%252F&cid=cl%3Aaffiliate%3A0qc2c4-tuscupones.com.mx&clickid=XGIUEfWHdxyKUB5VPCTaeUBZUkCQmZziRTdpSE0&irgwc=1',
-      title: 'Descuento en Lenovo',
-    },
-    {
-      id: 6,
-      imageUrl: require('../public/images/Emma.jpeg'),
-      link: 'https://www.emma-colchon.cl/combos/?awc=25386_1732158709_0d787cb4dcd0ef739db2644979019932&utm_source=awin-wwwsavingsunitedcom&utm_medium=affiliate&utm_campaign=413377&utm_content=LTKxzilCqz3WXK6NEdFdQHrCInu8UJA',
-      title: '70% OFF en Emma Conchones Chile',
-    },
-    {
-      id: 7,
-      imageUrl: require('../public/images/LG.jpeg'),
-      link: 'https://www.lg.com/cl/ofertas-smart-tv/?cjdata=MXxOfDB8WXww&cjevent=6cfc1cb1a7b611ef8274a6c20a82b82d&UTM_Source=CJ&UTM_id=5024114&UTM_campaigns=Savings+United+LATAM&UTM_Medium=affiliate',
-      title: '46% OFF LG',
-    },
-    {
-      id: 8,
-      imageUrl: require('../public/images/BarceloHotel.jpeg'),
-      link: 'https://www.barcelo.com/es-cl/ofertas/?utm_id=275885868_128489544&utm_campaign=latam_es_afl_bc_x_alwayson-x-x-x-x-x-x_co&c_content=textlink&c_campaign=alwayson&c_term=2988890_&utm_medium=afi&utm_source=barcelo_affiliates&dclid=CJDZ2ZW67IkDFSFNuAQdelkE3Q',
-      title: 'Hasta un 40% OFF en Barceló Hotel',
-    },
-    {
-      id: 9,
-      imageUrl: require('../public/images/Aliexpress.jpeg'),
-      link: 'https://best.aliexpress.com/?wh_offline=true&gatewayAdapt=glo2esp&browser_redirect=true',
-      title: '$4.800 de descuento en Aliexpress',
-    },
-  ];
-  
-  export default adsData;
-  
+export default adsData;
